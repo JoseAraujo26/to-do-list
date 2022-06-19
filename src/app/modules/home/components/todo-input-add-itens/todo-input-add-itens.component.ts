44858ec279f05f290dc199b78735331d5c804db0
José Emiliano <araujo_26@yahoo.com>
2022-06-19 00:23:44 -0300

    Funcionalidade de adicionar tarefa implementada

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-input-add-itens',
-  templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+	selector: 'app-todo-input-add-itens',
+	templateUrl: './todo-input-add-itens.component.html',
+	styleUrls: ['./todo-input-add-itens.component.scss']
 })
 export class TodoInputAddItensComponent implements OnInit {
 
-  constructor() { }
+	@Output() emitItemTaskList = new EventEmitter();
+	addItemTaskList = '';
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	submitItemTaskList() {
+		const text: string = this.formatInput(this.addItemTaskList);
+		if(text) {
+			this.emitItemTaskList.emit(text);
+			this.addItemTaskList = '';
+		}
+	}
+
+	private formatInput(text: string) {
+		return text.split(' ').filter((e: string) => e).join(' ');
+	}
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -2,31 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+	selector: 'app-todo-list',
+	templateUrl: './todo-list.component.html',
+	styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
 
-  taskList: TaskList[] = [
-    { task: 'Minha nova task', checked: true },
-    { task: 'Isso é um teste', checked: false },
-    { task: 'Terceira Task', checked: true },
-  ];
+	taskList: TaskList[] = [
+		{ task: 'Minha nova task', checked: true },
+		{ task: 'Isso é um teste', checked: false },
+		{ task: 'Terceira Task', checked: true },
+	];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  deleteItemTaskList(taskPosition: number) {
-    this.taskList.splice(taskPosition, 1);
-  }
+	setEmitTaskList(event: string) {
+		this.taskList.push({ task: event, checked: false });
+	}
 
-  deleteAll() {
-    const confirm = window.confirm('Você realmente deseja deletar tudo?')
-    if (confirm) {
-      this.taskList = [];
-    }
-  }
+	deleteItemTaskList(taskPosition: number) {
+		this.taskList.splice(taskPosition, 1);
+	}
+
+
+	deleteAll() {
+		const confirm = window.confirm('Você realmente deseja deletar tudo?');
+		if (confirm) {
+			this.taskList = [];
+		}
+	}
 }
